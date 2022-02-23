40e7e81507e26d884fdc546cabef66fe18d647fc
Álvaro Martínez-Cediel Algora <alvaro.martinez.algora@gmail.com>
2022-02-23 17:45:56 +0100

    un7manin

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Character;
+    public float Speed = 5f;
+
+    public bool canControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canControl == true)
+        {
+            float h = Input.GetAxis("Horizontal") * Speed;
+
+            Character.transform.Translate(h * Time.deltaTime, 0, 0);
+        }
+    }
+
+    
+}
---
+++ Assets/PositionSave.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSave : MonoBehaviour
+{
+    private float PlayerX;
+    private float PlayerY;
+    private float PlayerZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(PlayerPrefs.GetFloat("PlayerX"), PlayerPrefs.GetFloat("PlayerY"), PlayerPrefs.GetFloat("PlayerZ"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerPrefs.SetFloat("PlayerX", transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", transform.position.y);
+        PlayerPrefs.SetFloat("PlayerZ", transform.position.z);
+    }
+   
+}
